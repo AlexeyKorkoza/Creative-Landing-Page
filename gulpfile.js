71af2fc933a2458b87219751c3c2b40553814bfa
AlexeyKorkoza <alexeykorkoza@gmail.com>
2016-12-06 23:37:10 +0300

    Add gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,65 @@
-/**
- * Created by Alexey on 02.12.2016.
- */
+'use strict';
+
+var gulp = require('gulp'),
+  watch = require('gulp-watch'),
+  autoprefixer = require('gulp-autoprefixer'),
+  uglify = require('gulp-uglify'),
+  cssmin = require('gulp-minify-css'),
+  imagemin = require('gulp-imagemin'),
+  refresh = require('gulp-livereload'),
+  concat = require('gulp-concat'),
+  csslint = require('gulp-csslint'),
+  jscpd = require('gulp-jscpd'),
+  connect = require('gulp-connect'),
+  imageop = require('gulp-image-optimization');
+
+gulp.task('scripts', function() {
+  return gulp.src(['node_modules/jquery.2/node_modules/jquery/dist/jquery.js','js/*.js'])
+    .on('error', console.log)
+    .pipe(concat('scripts.min.js'))
+    .pipe(uglify())
+    .pipe(jscpd())
+    .pipe(gulp.dest('./build/'))
+    .pipe(connect.reload());
+});
+
+gulp.task('styles', function() {
+  return gulp.src('css/*.css')
+    .on('error', console.log)
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(cssmin())
+    .pipe(csslint())
+    .pipe(concat('style.min.css'))
+    .pipe(gulp.dest('./build/'))
+    .pipe(connect.reload());
+});
+
+gulp.task('images', function(){
+  return gulp.src('img/*')
+    .pipe(imageop({
+      optimizationLevel: 5,
+      progressive: true,
+      interlaced: true
+    }))
+    .pipe(gulp.dest('img/'))
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+    gulp.watch(['css/*'],['styles']);
+    gulp.watch(['js/*'],['scripts']);
+    gulp.watch(['img/*'],['images']);
+});
+
+gulp.task('connect', function () {
+  connect.server({
+    root: "",
+    port: 8080,
+    livereload: true
+  });
+});
+
+gulp.task('default', ['scripts', 'styles', 'watch', 'connect', 'images']);
